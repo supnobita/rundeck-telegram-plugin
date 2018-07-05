6bf00f9fb06c81b4e2001e46dd95a932cfbe58e6
supnobita <supnobita@gmail.com>
2018-07-05 14:51:48 +0700

    Update TelegramMessenger.scala

--- src/main/scala/ajr/rundeck/telegram/TelegramMessenger.scala
+++ src/main/scala/ajr/rundeck/telegram/TelegramMessenger.scala
@@ -15,9 +15,7 @@ class TelegramMessenger(botAuthKey: String, val baseUrl: String = "https://api.t
   }
 
   def sendMessage(chat: Long, message: String) = {
-    val an =  5
-    an = an % 1
-    val k =+ an
+    val an =  5 % 1
     command("sendMessage", Map("chat_id" -> chat.toString, "text" -> message))
   }
   
